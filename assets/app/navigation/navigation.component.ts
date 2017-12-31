958f17e576dbbfffd3d5c2c7c3d8fea7c5ec037a
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2017-12-31 15:50:57 +0700

    Add some style

--- assets/app/navigation/navigation.component.ts
+++ assets/app/navigation/navigation.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'navigation-app',
   templateUrl: 'navigation.component.html',
+  styleUrls:['./navigation.component.css'],
 })
 export class NavigationComponent {
-  
+
 }
